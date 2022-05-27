83c3213c06497ccff1079687c6c21c45cf22f514
XiaoQu <lqqu@thoughtworks.com>
2022-05-27 22:03:47 +0800

    feat: copy translate file

---
+++ src/utils/translate.js
@@ -0,0 +1,97 @@
+/**
+ * Created by Matheus Fernandes on 5/11/16.
+ */
+
+var querystring = require('querystring');
+var got = require('got');
+var tk = require('google-translate-api/tk');
+
+function translate(text, opts) {
+    opts = opts || {};
+    return tk(text).then(function (tk) {
+        var url = 'https://translate.google.com/translate_a/single';
+        var data = {
+            client: 'webapp',
+            sl: opts.from || 'auto',
+            tl: opts.to,
+            hl: opts.from || 'en',
+            dt: ['at', 'bd', 'ex', 'ld', 'md', 'qca', 'rw', 'rm', 'ss', 't'],
+            ie: 'UTF-8',
+            oe: 'UTF-8',
+            otf: 1,
+            ssel: 0,
+            tsel: 0,
+            kc: 7,
+            tk: opts.tk || tk,
+            q: text
+        };
+
+        return url + '?' + querystring.stringify(data);
+    }).then(function (url) {
+        return got(url).then(function (res) {
+            var result = {
+                text: '',
+                from: {
+                    language: {
+                        didYouMean: false,
+                        iso: ''
+                    },
+                    text: {
+                        autoCorrected: false,
+                        value: '',
+                        didYouMean: false
+                    }
+                },
+                raw: ''
+            };
+
+            if (opts.raw) {
+                result.raw = res.body;
+            }
+
+            var body = eval(res.body);
+            body[0] && body[0].forEach(function (obj) {
+                if (obj[0] != null) {
+                    result.text += obj[0];
+                }
+            });
+
+            if (body[2] === body[8][0][0]) {
+                result.from.language.iso = body[2];
+            } else {
+                result.from.language.didYouMean = true;
+                result.from.language.iso = body[8][0][0];
+            }
+
+            if (body[7] !== undefined && body[7][0] !== undefined) {
+                var str = body[7][0];
+
+                str = str.replace(/<b><i>/g, '[');
+                str = str.replace(/<\/i><\/b>/g, ']');
+
+                result.from.text.value = str;
+
+                if (body[7][5] === true) {
+                    result.from.text.autoCorrected = true;
+                } else {
+                    result.from.text.didYouMean = true;
+                }
+            }
+
+            return result;
+        }).catch(function (err) {
+            var e;
+            if (err.statusCode !== undefined && err.statusCode !== 200) {
+                e = new Error();
+                e.code = 'BAD_REQUEST';
+                throw e;
+            } else {
+                e = new Error();
+                e.code = 'BAD_NETWORK';
+                throw e;
+            }
+        });
+    });
+}
+
+module.exports = translate;
