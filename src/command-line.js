31f6bc6631068b92587cd9e0be0969433efae2fa
XiaoQu <lqqu@thoughtworks.com>
2022-05-28 16:35:06 +0800

    feat: finish function

--- src/command-line.js
+++ src/command-line.js
@@ -1,11 +1,34 @@
 const { translateToCN } = require('./index');
+const { program } = require('commander');
+const fs = require('fs');
 
-const fileName = process.argv.slice(2);
+program
+    .name("xiaoqu-translate")
+    .description("a tools for google translate")
+    .option("-f, --file <string>", "input file")
+    .version("1.0.0");
 
-const content = "i am a chinese man";
- translateToCN(content)
-     .then(res => {
-         console.log(res);
-     })
+program.parse();
+
+const filePath = program.opts().file
+
+const content = fs.readFileSync(filePath, { encoding: 'utf8' });
+
+const getFileWithExtension = filePath => {
+    const index = filePath.lastIndexOf('.');
+    return { path: filePath.substring(0, index), extension: filePath.substring(index, filePath.length) }
+}
+
+function getFileName(fileWithExtension) {
+    return `${ fileWithExtension.path }-zh_cn${ fileWithExtension.extension }`;
+}
+
+translateToCN(content)
+    .then(res => {
+        const fileWithExtension = getFileWithExtension(filePath);
+        let newFileName = getFileName(fileWithExtension);
+        fs.writeFileSync(newFileName, res)
+        console.log(`file:${ filePath },converted to ${ newFileName }`);
+    })
 
 
